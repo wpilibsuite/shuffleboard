ca26ce57016925cde71301b563f7a24043674a55
Sam Carlberg <sam.carlberg@gmail.com>
2017-05-19 22:17:51 -0400

    Make gradle project, add stub main class

---
+++ src/main/java/edu/wpi/first/shuffleboard/Main.java
@@ -0,0 +1,17 @@
+package edu.wpi.first.shuffleboard;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+  @Override
+  public void start(Stage primaryStage) {
+    primaryStage.show();
+  }
+
+}
