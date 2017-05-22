7c07c246f1a883bcebc1d266b70ccd3337419c7c
Sam Carlberg <sam.carlberg@gmail.com>
2017-05-22 19:03:34 -0400

    Move ThreadSafeProperty to its own upper-level class

---
+++ src/main/java/edu/wpi/first/shuffleboard/properties/ThreadSafeProperty.java
@@ -0,0 +1,54 @@
+package edu.wpi.first.shuffleboard.properties;
+
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ChangeListener;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static edu.wpi.first.shuffleboard.util.FxUtils.runOnFxThread;
+
+/**
+ * A thread-safe implementation of a property. Any changes to the value will execute on the JavaFX
+ * application thread.
+ */
+public class ThreadSafeProperty<T> extends SimpleObjectProperty<T> {
+
+  private final Map<ChangeListener<? super T>, ChangeListener<? super T>> wrappers = new HashMap<>();
+
+  public ThreadSafeProperty() {
+  }
+
+  public ThreadSafeProperty(T initialValue) {
+    super(initialValue);
+  }
+
+  public ThreadSafeProperty(Object bean, String name) {
+    super(bean, name);
+  }
+
+  public ThreadSafeProperty(Object bean, String name, T initialValue) {
+    super(bean, name, initialValue);
+  }
+
+  @Override
+  public void addListener(ChangeListener<? super T> listener) {
+    if (wrappers.containsKey(listener)) {
+      return;
+    }
+    wrappers.put(listener, ((obs, prev, cur) -> runOnFxThread(() -> listener.changed(obs, prev, cur))));
+    super.addListener(wrappers.get(listener));
+  }
+
+  @Override
+  public void removeListener(ChangeListener<? super T> listener) {
+    super.removeListener(wrappers.get(listener));
+    wrappers.remove(listener);
+  }
+
+  @Override
+  public void set(T newValue) {
+    runOnFxThread(() -> super.set(newValue));
+  }
+
+}
