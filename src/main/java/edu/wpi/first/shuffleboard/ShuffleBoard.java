6d890f9276ee9f72c089a9b49927de039720dc0f
Sam Carlberg <sam.carlberg@gmail.com>
2017-05-23 15:06:22 -0400

    Rename main class to ShuffleBoard

--- src/main/java/edu/wpi/first/shuffleboard/ShuffleBoard.java
+++ src/main/java/edu/wpi/first/shuffleboard/ShuffleBoard.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class Main extends Application {
+public class ShuffleBoard extends Application {
 
   public static void main(String[] args) {
     NetworkTable.setClientMode();
