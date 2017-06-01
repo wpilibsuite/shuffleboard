8581cd28c91ffe13a9962372e86182fabaca8471
Sam Carlberg <sam.carlberg@gmail.com>
2017-06-01 11:22:57 -0400

    Add "Number Slider" widget

---
+++ src/main/java/edu/wpi/first/shuffleboard/widget/NumberSlider.java
@@ -0,0 +1,30 @@
+package edu.wpi.first.shuffleboard.widget;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.Pane;
+
+@Description(
+    name = "Number Slider",
+    dataTypes = DataType.Number)
+@ParametrizedController("NumberSlider.fxml")
+public class NumberSlider extends Widget<Number> {
+
+  @FXML
+  private Pane root;
+  @FXML
+  private Slider slider;
+
+  @FXML
+  @Override
+  public void initialize() {
+    slider.majorTickUnitProperty().bind(slider.maxProperty().subtract(slider.minProperty()).divide(4));
+    exportProperties(slider.minProperty(), slider.maxProperty(), slider.blockIncrementProperty());
+    slider.valueProperty().bindBidirectional(source.dataProperty());
+  }
+
+  @Override
+  public Pane getView() {
+    return root;
+  }
+}
