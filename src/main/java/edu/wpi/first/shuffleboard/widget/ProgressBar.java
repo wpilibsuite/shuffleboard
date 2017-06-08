f3619182257aa1020331ee07d959a0cea11d4868
Sam Carlberg <sam.carlberg@gmail.com>
2017-06-08 19:35:42 -0400

    Add Progress Bar widget

---
+++ src/main/java/edu/wpi/first/shuffleboard/widget/ProgressBar.java
@@ -0,0 +1,46 @@
+package edu.wpi.first.shuffleboard.widget;
+
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+
+@Description(name = "Progress Bar", dataTypes = DataType.Number)
+@ParametrizedController("ProgressBar.fxml")
+public class ProgressBar extends SimpleAnnotatedWidget<Number> {
+
+  @FXML
+  private Pane root;
+  @FXML
+  private javafx.scene.control.ProgressBar progressBar;
+
+  private final DoubleProperty minValue = new SimpleDoubleProperty(this, "Min Value", 0);
+  private final DoubleProperty maxValue = new SimpleDoubleProperty(this, "Max Value", 1);
+
+  @FXML
+  private void initialize() {
+    progressBar.progressProperty().bind(Bindings.createDoubleBinding(this::calculateProgress,
+        dataProperty(), minValue, maxValue));
+    exportProperties(minValue, maxValue);
+  }
+
+  private double calculateProgress() {
+    if (getData() == null) {
+      return -1;
+    }
+    final double min = minValue.get();
+    final double max = maxValue.get();
+    final double value = getData().doubleValue();
+    if (min >= max || value < min || value > max) {
+      return -1;
+    }
+    return (value - min) / (max - min);
+  }
+
+  @Override
+  public Pane getView() {
+    return root;
+  }
+
+}
