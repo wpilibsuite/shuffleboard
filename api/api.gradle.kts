ac032dc8d7a00a3ab978dedeb579f3cde971c8e6
Sam Carlberg <sam.carlberg@gmail.com>
2017-08-17 16:41:22 -0400

    Implementation dependency on ntcore-jni

--- api/api.gradle.kts
+++ api/api.gradle.kts
@@ -12,6 +12,5 @@ dependencies {
     api(group = "org.controlsfx", name = "controlsfx", version = "8.40.11")
     api(group = "edu.wpi.first.ntcore", name = "ntcore-java", version = "3.1.7-20170808143930-12-gccfeab5")
     implementation(group = "edu.wpi.first.wpiutil", name = "wpiutil-java", version = "2.0.0-20170808143537-16-gf0cc5d9")
-    runtime(group = "edu.wpi.first.ntcore", name = "ntcore-jni", version = "3.1.7-20170808143930-12-gccfeab5", classifier = "all")
-    testRuntime(group = "edu.wpi.first.ntcore", name = "ntcore-jni", version = "3.1.7-20170808143930-12-gccfeab5", classifier = "all")
+    implementation(group = "edu.wpi.first.ntcore", name = "ntcore-jni", version = "3.1.7-20170808143930-12-gccfeab5", classifier = "all")
 }
