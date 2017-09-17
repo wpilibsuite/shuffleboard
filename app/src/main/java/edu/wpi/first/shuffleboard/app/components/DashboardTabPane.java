cae1d6e1e70e835574cc67e72e98884ee096912a
evan <eg1290@gmail.com>
2017-09-17 18:07:49 -0400

    Fix #124
    When PropertySheet has padding, it doesn't calculate it's height correctly.
    Dialogs give some padding to their content
    So PropertySheets directly embedded in Dialogs are too short

--- app/src/main/java/edu/wpi/first/shuffleboard/app/components/DashboardTabPane.java
+++ app/src/main/java/edu/wpi/first/shuffleboard/app/components/DashboardTabPane.java
@@ -29,6 +29,7 @@ import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.layout.BorderPane;
 
 import static edu.wpi.first.shuffleboard.api.util.TypeUtils.optionalCast;
 
@@ -169,7 +170,9 @@ public class DashboardTabPane extends TabPane {
         dialog.getDialogPane().getStylesheets().setAll(AppPreferences.getInstance().getTheme().getStyleSheets());
         dialog.setResizable(true);
         dialog.titleProperty().bind(EasyBind.map(this.title, t -> t + " Preferences"));
-        dialog.getDialogPane().setContent(propertySheet);
+
+        // property sheet can't be used directly in dialogs because it doesn't respect padding
+        dialog.getDialogPane().setContent(new BorderPane(propertySheet));
         dialog.getDialogPane().getButtonTypes().addAll(ButtonType.CLOSE);
         dialog.setOnCloseRequest(__ -> {
           this.sourcePrefix.setValue(dummySourcePrefix.getValue());
