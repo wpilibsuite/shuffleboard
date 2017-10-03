6ebd560075478fbc3d8f5bec7a4a7ca9ee150f30
nightpool <eg1290@gmail.com>
2017-10-03 14:47:52 -0400

    Revert "Revert "Switches shuffleboard to new ntcore artifact locations (#111)" (#218)" (#219)
    This reverts commit d80cf105825150b62513eeeaf5a000afd51bbaa7.

--- api/api.gradle.kts
+++ api/api.gradle.kts
@@ -11,5 +11,7 @@ dependencies {
     api(group = "org.fxmisc.easybind", name = "easybind", version = "1.0.3")
     api(group = "org.controlsfx", name = "controlsfx", version = "8.40.11")
     api(group = "de.codecentric.centerdevice", name = "javafxsvg", version = "1.2.1")
-    api(group = "edu.wpi.first.wpilib.networktables.java", name = "NetworkTables", version = "+", classifier = "desktop")
+    api(group = "edu.wpi.first.ntcore", name = "ntcore-java", version = "3.1.7-20170808143930-12-gccfeab5")
+    implementation(group = "edu.wpi.first.wpiutil", name = "wpiutil-java", version = "2.0.0-20170808143537-16-gf0cc5d9")
+    implementation(group = "edu.wpi.first.ntcore", name = "ntcore-jni", version = "3.1.7-20170808143930-12-gccfeab5", classifier = "all")
 }
