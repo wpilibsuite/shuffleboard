35d28238bc2499ac6d4676742ed5b50983cb3237
Sam Carlberg <sam.carlberg@gmail.com>
2017-10-11 21:33:14 -0400

    Fix subsystem population (#234)
    * Fix subsystems adding widgets for data that already has a parent
    
    * Fix empty space at the tops of subsystem layouts
    
    Was caused by adding empty subsystem layouts for the same source

--- plugins/base/src/main/java/edu/wpi/first/shuffleboard/plugin/base/layout/SubsystemLayout.java
+++ plugins/base/src/main/java/edu/wpi/first/shuffleboard/plugin/base/layout/SubsystemLayout.java
@@ -107,7 +107,9 @@ public class SubsystemLayout implements Layout, Populatable, Sourced {
 
   @Override
   public boolean supports(DataSource<?> source) {
-    return this.source != null && source.getName().startsWith(getSource().getName());
+    return this.source != null
+        && this.source != source
+        && source.getName().startsWith(getSource().getName());
   }
 
   @Override
@@ -116,7 +118,7 @@ public class SubsystemLayout implements Layout, Populatable, Sourced {
         .flatMap(TypeUtils.castStream(Sourced.class))
         .map(Sourced::getSource)
         .map(DataSource::getId)
-        .anyMatch(uri -> uri.startsWith(source.getId()));
+        .anyMatch(source.getId()::startsWith);
   }
 
   @Override
