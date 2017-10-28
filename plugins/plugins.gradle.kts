5b5b5ea39f9cca11c3e97484541a75674fdcdf55
Sam Carlberg <sam.carlberg@gmail.com>
2017-10-28 18:22:34 -0400

    Publish API and plugin jars and source and javadocs for all artifacts
    Fixes #193
    Fixes #265
    
    Plugins are currently distributed as normal jars, (not as uberjars). They only work because the app jar bundles their dependencies as transitive dependencies.

--- plugins/plugins.gradle.kts
+++ plugins/plugins.gradle.kts
@@ -1,7 +1,18 @@
+import edu.wpi.first.wpilib.versioning.ReleaseType
+import org.gradle.jvm.tasks.Jar
+
+// Ensure that the WPILibVersioningPlugin is setup by setting the release type, if releaseType wasn't
+// already specified on the command line
+if (!hasProperty("releaseType")) {
+    WPILibVersion {
+        releaseType = ReleaseType.DEV
+    }
+}
+
 subprojects {
-    apply {
-        plugin("com.github.johnrengelman.shadow")
-        plugin("java-library")
+    plugins {
+        id("edu.wpi.first.wpilib.versioning.WPILibVersioningPlugin") version "2.0"
+        `java-library`
     }
     dependencies {
         compile(group = "com.google.code.findbugs", name = "annotations", version = "+")
@@ -12,4 +23,32 @@ subprojects {
         implementation(group = "edu.wpi.first.cscore", name = "cscore-jni", version = "+", classifier = "all")
         implementation(group = "org.opencv", name = "opencv-jni", version = "+", classifier = "all")
     }
+    val sourceJar = task<Jar>("sourceJar") {
+        description = "Creates a JAR that contains the source code."
+        from(java.sourceSets["main"].allSource)
+        classifier = "sources"
+    }
+    val javadocJar = task<Jar>("javadocJar") {
+        dependsOn("javadoc")
+        description = "Creates a JAR that contains the javadocs."
+        from(java.docsDir)
+        classifier = "javadoc"
+    }
+    publishing.publications {
+        create<MavenPublication>("api") {
+            groupId = "edu.wpi.first.shuffleboard.plugin"
+            artifactId = project.name
+            getWPILibVersion()?.let { version = it }
+            val jar: Jar by tasks
+            artifact(jar)
+            artifact(javadocJar)
+            artifact(sourceJar)
+        }
+    }
 }
+
+/**
+ * @return [edu.wpi.first.wpilib.versioning.WPILibVersioningPluginExtension.version] value or null
+ * if that value is the empty string.
+ */
+fun getWPILibVersion(): String? = if (WPILibVersion.version != "") WPILibVersion.version else null
