5b264a4e8a862c047ea81085f6ae30698be0a097
Sam Carlberg <sam.carlberg@gmail.com>
2017-10-29 18:05:45 -0400

    Publish API and plugin jars and source and javadocs for all artifacts (#266)
    * Publish API and plugin jars and source and javadocs for all artifacts
    
    Fixes #193
    Fixes #265
    
    * Fix plugin publishing
    
    Publishes normal jars, but lists dependencies in the POM
    Fixed settings.gradle only working for first-level gradle.kts files

--- plugins/cameraserver/cameraserver.gradle.kts
+++ plugins/cameraserver/cameraserver.gradle.kts
@@ -2,12 +2,10 @@ description = """
 The bundled CameraServer plugin. This plugin provides data sources and widgets for viewing MJPEG streams from the WPILib CameraServer.
 """.trimIndent().trim()
 
-plugins {
-    id("edu.wpi.first.wpilib.versioning.WPILibVersioningPlugin") version "1.6"
-}
-
 dependencies {
-    api(group = "edu.wpi.first.cscore", name = "cscore-java", version = "+")
-    implementation(group = "edu.wpi.first.cscore", name = "cscore-jni", version = "+")
-    implementation(group = "edu.wpi.first.wpilib", name = "opencv", version = "3.2.0")
+    compile(group = "edu.wpi.first.cscore", name = "cscore-java", version = "+")
+    compile(group = "org.opencv", name = "opencv-java", version = "3.2.0")
+    compile(group = "org.jcodec", name = "jcodec-javase", version = "0.2.0")
+    compile(group = "edu.wpi.first.cscore", name = "cscore-jni", version = "+", classifier = "all")
+    compile(group = "org.opencv", name = "opencv-jni", version = "+", classifier = "all")
 }
