31b541c46fb5e39c8363a70365ba3b05ff4fb729
Sam Carlberg <sam.carlberg@gmail.com>
2017-10-28 22:25:08 -0400

    Fix plugin publishing
    Publishes normal jars, but lists dependencies in the POM
    Fixed settings.gradle only working for first-level gradle.kts files

--- plugins/cameraserver/cameraserver.gradle.kts
+++ plugins/cameraserver/cameraserver.gradle.kts
@@ -3,7 +3,9 @@ The bundled CameraServer plugin. This plugin provides data sources and widgets f
 """.trimIndent().trim()
 
 dependencies {
-    api(group = "edu.wpi.first.cscore", name = "cscore-java", version = "+")
-    implementation(group = "edu.wpi.first.cscore", name = "cscore-jni", version = "+")
-    implementation(group = "edu.wpi.first.wpilib", name = "opencv", version = "3.2.0")
+    compile(group = "edu.wpi.first.cscore", name = "cscore-java", version = "+")
+    compile(group = "org.opencv", name = "opencv-java", version = "3.2.0")
+    compile(group = "org.jcodec", name = "jcodec-javase", version = "0.2.0")
+    compile(group = "edu.wpi.first.cscore", name = "cscore-jni", version = "+", classifier = "all")
+    compile(group = "org.opencv", name = "opencv-jni", version = "+", classifier = "all")
 }
--- plugins/plugins.gradle.kts
+++ plugins/plugins.gradle.kts
@@ -1,48 +1,43 @@
 import edu.wpi.first.wpilib.versioning.ReleaseType
 import org.gradle.jvm.tasks.Jar
 
-// Ensure that the WPILibVersioningPlugin is setup by setting the release type, if releaseType wasn't
-// already specified on the command line
-if (!hasProperty("releaseType")) {
-    WPILibVersion {
-        releaseType = ReleaseType.DEV
-    }
-}
-
 subprojects {
-    plugins {
-        id("edu.wpi.first.wpilib.versioning.WPILibVersioningPlugin") version "2.0"
-        `java-library`
-    }
-    dependencies {
-        compile(group = "com.google.code.findbugs", name = "annotations", version = "+")
-        compile(project(":api"))
-        compile(group = "edu.wpi.first.cscore", name = "cscore-java", version = "+")
-        compile(group = "org.opencv", name = "opencv-java", version = "3.2.0")
-        compile(group = "org.jcodec", name = "jcodec-javase", version = "0.2.0")
-        implementation(group = "edu.wpi.first.cscore", name = "cscore-jni", version = "+", classifier = "all")
-        implementation(group = "org.opencv", name = "opencv-jni", version = "+", classifier = "all")
-    }
-    val sourceJar = task<Jar>("sourceJar") {
-        description = "Creates a JAR that contains the source code."
-        from(java.sourceSets["main"].allSource)
-        classifier = "sources"
-    }
-    val javadocJar = task<Jar>("javadocJar") {
-        dependsOn("javadoc")
-        description = "Creates a JAR that contains the javadocs."
-        from(java.docsDir)
-        classifier = "javadoc"
-    }
-    publishing.publications {
-        create<MavenPublication>("api") {
-            groupId = "edu.wpi.first.shuffleboard.plugin"
-            artifactId = project.name
-            getWPILibVersion()?.let { version = it }
-            val jar: Jar by tasks
-            artifact(jar)
-            artifact(javadocJar)
-            artifact(sourceJar)
+    afterEvaluate {
+        // Ensure that the WPILibVersioningPlugin is setup by setting the release type, if releaseType wasn't
+        // already specified on the command line
+        if (!hasProperty("releaseType")) {
+            WPILibVersion {
+                releaseType = ReleaseType.DEV
+            }
+        }
+        plugins {
+            id("edu.wpi.first.wpilib.versioning.WPILibVersioningPlugin") version "2.0"
+            `java-library`
+        }
+        dependencies {
+            compileOnly(group = "com.google.code.findbugs", name = "annotations", version = "+")
+            compile(project(":api"))
+        }
+        val sourceJar = task<Jar>("sourceJar") {
+            description = "Creates a JAR that contains the source code."
+            from(java.sourceSets["main"].allSource)
+            classifier = "sources"
+        }
+        val javadocJar = task<Jar>("javadocJar") {
+            dependsOn("javadoc")
+            description = "Creates a JAR that contains the javadocs."
+            from(java.docsDir)
+            classifier = "javadoc"
+        }
+        publishing.publications {
+            create<MavenPublication>("plugin.${project.name}") {
+                groupId = "edu.wpi.first.shuffleboard.plugin"
+                artifactId = project.name
+                getWPILibVersion()?.let { version = it }
+                from(components["java"])
+                artifact(javadocJar)
+                artifact(sourceJar)
+            }
         }
     }
 }
