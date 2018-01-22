7fe5cb7cec049b14bc566aae2ed2682b085be12d
Sam Carlberg <sam.carlberg@gmail.com>
2018-01-22 17:47:18 -0500

    Save grid visibility preferences (#381)
    Does not break backwards compatibility with old save fies

--- app/src/main/java/edu/wpi/first/shuffleboard/app/json/WidgetPaneSaver.java
+++ app/src/main/java/edu/wpi/first/shuffleboard/app/json/WidgetPaneSaver.java
@@ -15,6 +15,7 @@ import com.google.gson.JsonSerializationContext;
 
 import java.lang.reflect.Type;
 import java.util.Map;
+import java.util.Optional;
 
 import javafx.scene.layout.GridPane;
 
@@ -25,6 +26,7 @@ public class WidgetPaneSaver implements ElementTypeAdapter<WidgetPane> {
   public JsonElement serialize(WidgetPane src, JsonSerializationContext context) {
     JsonObject object = new JsonObject();
     object.addProperty("gridSize", src.getTileSize());
+    object.addProperty("showGrid", src.isShowGrid());
     object.addProperty("hgap", src.getHgap());
     object.addProperty("vgap", src.getVgap());
     JsonObject tiles = new JsonObject();
@@ -49,6 +51,7 @@ public class WidgetPaneSaver implements ElementTypeAdapter<WidgetPane> {
   public WidgetPane deserialize(JsonElement json, JsonDeserializationContext context) throws JsonParseException {
     JsonObject object = json.getAsJsonObject();
     WidgetPane pane = new WidgetPane();
+    pane.setShowGrid(Optional.ofNullable(object.get("showGrid")).map(JsonElement::getAsBoolean).orElse(true));
     pane.setTileSize(object.get("gridSize").getAsDouble());
     pane.setHgap(object.get("hgap").getAsDouble());
     pane.setVgap(object.get("vgap").getAsDouble());
