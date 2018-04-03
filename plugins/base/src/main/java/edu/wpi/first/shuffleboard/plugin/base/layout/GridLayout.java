c9f1785b21d92694bc3a164a547325d60bde4a5d
Sam Carlberg <sam.carlberg@gmail.com>
2018-04-03 15:26:53 -0400

    Use a grid instead of table
    Allows for more columns

--- plugins/base/src/main/java/edu/wpi/first/shuffleboard/plugin/base/layout/GridLayout.java
+++ plugins/base/src/main/java/edu/wpi/first/shuffleboard/plugin/base/layout/GridLayout.java
@@ -4,100 +4,126 @@ import edu.wpi.first.shuffleboard.api.components.ActionList;
 import edu.wpi.first.shuffleboard.api.components.EditableLabel;
 import edu.wpi.first.shuffleboard.api.util.ListUtils;
 import edu.wpi.first.shuffleboard.api.widget.Component;
-import edu.wpi.first.shuffleboard.api.widget.Layout;
 import edu.wpi.first.shuffleboard.api.widget.LayoutBase;
 import edu.wpi.first.shuffleboard.api.widget.ParametrizedController;
 
-import javafx.beans.property.ReadOnlyObjectWrapper;
+import com.google.common.collect.ImmutableList;
+
+import org.fxmisc.easybind.EasyBind;
+import org.fxmisc.easybind.Subscription;
+
+import java.util.List;
+import java.util.Map;
+import java.util.WeakHashMap;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.Property;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
-import javafx.scene.control.TableCell;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 
 @ParametrizedController("GridLayout.fxml")
-public class GridLayout extends LayoutBase implements Layout {
+public class GridLayout extends LayoutBase {
+
+  private final ObservableList<Component> components = FXCollections.observableArrayList();
 
   @FXML
   private Pane root;
   @FXML
-  private TableView<Component> table;
-  @FXML
-  private TableColumn<Component, String> titleColumn;
-  @FXML
-  private TableColumn<Component, Node> viewColumn;
+  private GridPane grid;
+
+  private final IntegerProperty numColumns = new SimpleIntegerProperty(this, "numColumns", 1);
+  private final IntegerProperty numRows = new SimpleIntegerProperty(this, "numRows", 1);
+  private final Map<Component, Pane> panes = new WeakHashMap<>();
 
   @FXML
   private void initialize() {
-    titleColumn.setCellValueFactory(f -> f.getValue().titleProperty());
-    titleColumn.setCellFactory(c -> new TableCell<Component, String>() {
-
-      private final EditableLabel label = new EditableLabel();
-
-      {
-        label.textProperty().addListener((__, old, text) -> {
-          Component component = getComponent();
-          if (component != null) {
-            component.setTitle(text);
+    components.addListener((ListChangeListener<Component>) c -> {
+      while (c.next()) {
+        if (c.wasReplaced()) {
+          // ignore
+          for (int i = 0; i < c.getAddedSize(); i++) {
+            Component removed = c.getRemoved().get(i);
+            Component added = c.getAddedSubList().get(i);
+            Pane oldPane = panes.remove(removed);
+            Pane newPane = paneFor(added);
+            GridPane.setRowIndex(newPane, GridPane.getRowIndex(oldPane));
+            GridPane.setColumnIndex(newPane, GridPane.getColumnIndex(oldPane));
+            ListUtils.replaceIn(grid.getChildren())
+                .replace(oldPane)
+                .with(newPane);
           }
-        });
-      }
-
-      @Override
-      protected void updateItem(String item, boolean empty) {
-        super.updateItem(item, empty);
-        if (empty) {
-          setGraphic(null);
-        } else {
-          label.setText(item);
-          setGraphic(label);
-        }
-      }
-
-      Component getComponent() {
-        if (getTableView() == null) {
-          return null;
-        }
-        if (getTableRow() == null) {
-          return null;
-        }
-        if (getTableRow().getIndex() < 0) {
-          return null;
-        }
-        return getTableView().getItems().get(getTableRow().getIndex());
-      }
-    });
-
-    viewColumn.setCellValueFactory(f -> new ReadOnlyObjectWrapper<>(f.getValue().getView()));
-    viewColumn.setCellFactory(c -> new TableCell<Component, Node>() {
-      @Override
-      protected void updateItem(Node item, boolean empty) {
-        super.updateItem(item, empty);
-        if (empty) {
-          setGraphic(null);
-        } else {
-          setGraphic(item);
+        } else if (c.wasAdded()) {
+          for (Component added : c.getAddedSubList()) {
+            Pane pane = paneFor(added);
+            boolean anyOpen = false;
+            for (int col = 0; col < numColumns.get() && !anyOpen; col++) {
+              for (int row = 0; row < numRows.get(); row++) {
+                if (isOpen(col, row)) {
+                  GridPane.setColumnIndex(pane, col);
+                  GridPane.setRowIndex(pane, row);
+                  anyOpen = true;
+                  break;
+                }
+              }
+            }
+            if (!anyOpen) {
+              // Add another row
+              numRows.set(numRows.get() + 1);
+              GridPane.setRowIndex(pane, numRows.get() - 1);
+              GridPane.setColumnIndex(pane, 0);
+            }
+            grid.getChildren().add(pane);
+          }
+        } else if (c.wasRemoved()) {
+          c.getRemoved().stream()
+              .map(panes::remove)
+              .forEach(grid.getChildren()::remove);
         }
       }
     });
-    viewColumn.setSortable(false); // Views don't have any sensible sort order
+  }
+
+  private boolean isOpen(int col, int row) {
+    return grid.getChildren().stream()
+        .filter(n -> GridPane.getColumnIndex(n) != null)
+        .noneMatch(n -> GridPane.getColumnIndex(n) == col && GridPane.getRowIndex(n) == row);
+  }
+
+  private Pane paneFor(Component component) {
+    if (panes.containsKey(component)) {
+      return panes.get(component);
+    }
+    BorderPane pane = new BorderPane();
+    pane.getStyleClass().add("layout-stack");
+    pane.setCenter(component.getView());
+    EditableLabel label = new EditableLabel(component.titleProperty());
+    label.getStyleClass().add("layout-label");
+    pane.setLeft(label);
+    GridPane.setRowIndex(pane, numRows.get() - 1);
+    ActionList.registerSupplier(pane, () -> actionsForComponent(component));
+    panes.put(component, pane);
+    return pane;
   }
 
   @Override
   protected void addComponentToView(Component component) {
-    table.getItems().add(component);
-    ActionList.registerSupplier(component.getView(), () -> actionsForComponent(component));
+    components.add(component);
   }
 
   @Override
   protected void removeComponentFromView(Component component) {
-    table.getItems().remove(component);
+    components.remove(component);
   }
 
   @Override
   protected void replaceInPlace(Component existing, Component replacement) {
-    ListUtils.replaceIn(table.getItems())
+    ListUtils.replaceIn(components)
         .replace(existing)
         .with(replacement);
   }
@@ -105,6 +131,14 @@ public class GridLayout extends LayoutBase implements Layout {
   @Override
   public Pane getView() {
     return root;
+  }
+
+  @Override
+  public List<Property<?>> getProperties() {
+    return ImmutableList.of(
+        numColumns,
+        numRows
+    );
   }
 
   @Override
