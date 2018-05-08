c3d1cacc18b3b7b8d6405e900d453f03fb5f2bee
Sam Carlberg <sam.carlberg@gmail.com>
2018-05-08 13:27:34 -0400

    Static preloader background
    Since startup time is so fast, the animations don't have enough time to play
    If startup takes so long that the animations would be apparent, the system is probably too slow to be running animations on anyway

--- app/src/main/java/edu/wpi/first/shuffleboard/app/PreloaderController.java
+++ app/src/main/java/edu/wpi/first/shuffleboard/app/PreloaderController.java
@@ -6,25 +6,16 @@ import org.controlsfx.tools.Utils;
 
 import java.util.Random;
 
-import javafx.animation.Animation;
-import javafx.animation.FillTransition;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import javafx.util.Duration;
-
-import static org.controlsfx.tools.Utils.clamp;
 
 public class PreloaderController {
 
   private static final Color primaryColor = Color.rgb(39, 39, 247);
   private static final Color secondaryColor = Color.rgb(64, 64, 255);
-
-  // Animation timings
-  private static final double minTime = 0.75;
-  private static final double maxTime = 1.25;
 
   private static final double HEXAGON_RADIUS = 10;
 
@@ -47,23 +38,18 @@ public class PreloaderController {
     progressBar.setProgress(-1);
     versionLabel.setText(Shuffleboard.getVersion());
 
-    // Animated hexagon grid background
+    // Hexagon grid background
     Random random = new Random(System.currentTimeMillis() ^ (System.currentTimeMillis() >> 16));
     HexagonGrid hexagonGrid = new HexagonGrid(
         (int) (root.getPrefWidth() / HEXAGON_RADIUS),
         (int) (root.getPrefHeight() / HEXAGON_RADIUS),
         HEXAGON_RADIUS,
         0);
-    // animate the hexagons
-    hexagonGrid.hexagons()
-        .stream()
-        .map(h -> new FillTransition(
-            Duration.seconds(
-                clamp(0, random.nextGaussian() + 1, 2) * (maxTime - minTime) + minTime),
-            h, primaryColor, secondaryColor))
-        .peek(t -> t.setCycleCount(Animation.INDEFINITE))
-        .peek(t -> t.setAutoReverse(true))
-        .forEach(t -> t.playFrom(Duration.seconds(random.nextDouble() * maxTime)));
+
+    // Set colors randomly
+    hexagonGrid.hexagons().forEach(hexagon -> {
+      hexagon.setFill(primaryColor.interpolate(secondaryColor, random.nextDouble()));
+    });
     backgroundContainer.getChildren().setAll(hexagonGrid);
 
     FxUtils.setController(root, this);
