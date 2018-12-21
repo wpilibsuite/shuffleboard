02556280a938924c1b93e46f886916551d0ab0ba
Sam Carlberg <sam.carlberg@gmail.com>
2018-12-21 17:27:26 -0500

    Add event markers to exported CSV files

--- api/src/main/java/edu/wpi/first/shuffleboard/api/sources/recording/Converter.java
+++ api/src/main/java/edu/wpi/first/shuffleboard/api/sources/recording/Converter.java
@@ -1,7 +1,16 @@
 package edu.wpi.first.shuffleboard.api.sources.recording;
 
+import edu.wpi.first.shuffleboard.api.sources.DataSourceUtils;
+
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Handles converting of recording files to a file. These are useful for making it easier to analyze recorded data with
@@ -31,5 +40,92 @@ public interface Converter {
    * @throws IOException if the file could not be written
    */
   void export(Recording recording, Path destination, ConversionSettings settings) throws IOException;
+
+  /**
+   * Flattens recording entries into a single map of timestamp-to-data. Note that each timestamp is expected to have
+   * no more than ONE event marker mapped to it.
+   *
+   * @param recording the recording to flatten
+   * @param settings  the conversion settings to use
+   * @param window    the time window within which temporally-close data should be considered to have the same
+   *                  timestamp. A value of zero will result in only entries for that timestamp being mapped to it;
+   *                  higher values loosens this restriction to accommodate potential variances in timestamps.
+   *
+   * @return a flattened view the timestamped entries in the recording
+   *
+   * @throws IllegalArgumentException if {@code window} is negative
+   */
+  @SuppressWarnings("LocalVariableName")
+  static Map<Long, List<RecordingEntry>> flatten(Recording recording, ConversionSettings settings, long window) {
+    if (window < 0) {
+      throw new IllegalArgumentException("Time window must be non-negative, given " + window);
+    }
+
+    if (recording.getData().isEmpty() && recording.getMarkers().isEmpty()) {
+      // No data or events
+      return Map.of();
+    }
+
+    // All the timestamped data and event markers, sorted by timestamp in ascending order (oldest data first)
+    final List<RecordingEntry> data = Stream.concat(
+        recording.getData().stream(),
+        recording.getMarkers().stream())
+        .sorted(Comparator.comparingLong(RecordingEntry::getTimestamp))
+        .collect(Collectors.toList());
+
+    final Map<Long, List<RecordingEntry>> map = new HashMap<>();
+    final boolean skipMetadata = !settings.isConvertMetadata();
+
+    for (int i = 0; i < data.size(); ) {
+      RecordingEntry point = data.get(i);
+      if (skipMetadata && isMetadata(point)) {
+        // Skip metadata
+        i++;
+        continue;
+      }
+
+      int j = i;
+      List<RecordingEntry> elements = new ArrayList<>();
+      // Collate data within a certain delta time to the same collection, since there may be some time jitter
+      // for multiple recorded data points that were updated at the same time, but network latencies
+      // or CPU usage caused the timestamps to be slightly different
+      for (; j < data.size() && data.get(j).getTimestamp() <= point.getTimestamp() + window; j++) {
+        var e = data.get(j);
+        if (skipMetadata && isMetadata(e)) {
+          continue;
+        }
+        elements.add(e);
+      }
+
+      // Place the marker (if present) at the beginning
+      // Note: assumes only one marker is present per slice, so the time window must
+      // be narrow enough to preserve this behavior
+      for (int k = elements.size() - 1; k >= 0; k--) {
+        if (elements.get(k) instanceof Marker) {
+          var element = elements.remove(k);
+          elements.add(0, element);
+          break;
+        }
+      }
+      map.put(point.getTimestamp(), elements);
+      i = j;
+    }
+    return map;
+  }
+
+  /**
+   * Checks if a recording entry is metadata.
+   *
+   * @param entry the entry to check
+   *
+   * @return true if the entry is metadata, false if not
+   */
+  private static boolean isMetadata(RecordingEntry entry) {
+    if (entry instanceof TimestampedData) {
+      var data = (TimestampedData) entry;
+      return DataSourceUtils.isMetadata(data.getSourceId());
+    }
+    return false;
+  }
 
 }
--- app/src/main/java/edu/wpi/first/shuffleboard/app/sources/recording/CsvConverter.java
+++ app/src/main/java/edu/wpi/first/shuffleboard/app/sources/recording/CsvConverter.java
@@ -3,7 +3,9 @@ package edu.wpi.first.shuffleboard.app.sources.recording;
 import edu.wpi.first.shuffleboard.api.sources.DataSourceUtils;
 import edu.wpi.first.shuffleboard.api.sources.recording.ConversionSettings;
 import edu.wpi.first.shuffleboard.api.sources.recording.Converter;
+import edu.wpi.first.shuffleboard.api.sources.recording.Marker;
 import edu.wpi.first.shuffleboard.api.sources.recording.Recording;
+import edu.wpi.first.shuffleboard.api.sources.recording.RecordingEntry;
 import edu.wpi.first.shuffleboard.api.sources.recording.TimestampedData;
 import edu.wpi.first.shuffleboard.api.util.AlphanumComparator;
 
@@ -15,10 +17,15 @@ import org.apache.commons.csv.CSVPrinter;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
-import java.io.Writer;
+import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 public final class CsvConverter implements Converter {
 
@@ -30,6 +37,10 @@ public final class CsvConverter implements Converter {
   // This value is set to be able to collate data that gets updated 100 times per second without
   // pulling data from two separate update events into a single row
   private static final int TIME_WINDOW = 7; // milliseconds
+
+  private static final Logger log = Logger.getLogger(CsvConverter.class.getName());
+  private static final String invariantViolatedMessageFormat =
+      "Invariant violated: multiple non-data points in data list (found: %s at index %d of %d), for timestamp %d";
 
   private CsvConverter() {
   }
@@ -45,43 +56,85 @@ public final class CsvConverter implements Converter {
   }
 
   @Override
-  @SuppressWarnings("LocalVariableName")
-  @SuppressFBWarnings(value = "UC_USELESS_OBJECT", justification = "False positive with List.forEach")
   public void export(Recording recording, Path destination, ConversionSettings settings) throws IOException {
+    try (var outputStream = new FileOutputStream(destination.toFile());
+         var writer = new OutputStreamWriter(outputStream, StandardCharsets.UTF_8)) {
+      writer.append(convertToCsv(recording, settings));
+    }
+  }
+
+  /**
+   * Converts recorded data to CSV text.
+   *
+   * @param recording the recording to convert
+   * @param settings  the conversion settings to use
+   *
+   * @return a CSV-formatted text string of the data in the recording
+   */
+  @SuppressFBWarnings(value = "UC_USELESS_OBJECT", justification = "False positive with List.forEach")
+  public String convertToCsv(Recording recording, ConversionSettings settings) {
     List<String> header = makeHeader(recording, settings);
     int headerSize = header.size();
     CSVFormat csvFormat = CSVFormat.DEFAULT.withHeader(header.toArray(new String[headerSize]));
 
-    try (Writer writer = new OutputStreamWriter(new FileOutputStream(destination.toFile()), "UTF-8");
-         CSVPrinter printer = new CSVPrinter(writer, csvFormat)) {
+    try (var writer = new StringWriter();
+         var csvPrinter = new CSVPrinter(writer, csvFormat)) {
+      var flattenedData = Converter.flatten(recording, settings, TIME_WINDOW);
 
-      List<TimestampedData> data = recording.getData();
-      boolean skipMetadata = !settings.isConvertMetadata();
+      var rows = flattenedData.entrySet()
+          .stream()
+          .map(e -> toRow(header, headerSize, e))
+          .filter(Objects::nonNull)
+          .collect(Collectors.toList());
 
-      for (int i = 0; i < data.size(); ) {
-        TimestampedData point = data.get(i);
-        if (skipMetadata && DataSourceUtils.isMetadata(point.getSourceId())) {
-          i++;
-          continue;
-        }
-        int j = i;
-        List<TimestampedData> row = new ArrayList<>();
-        // Collate data within a certain delta time to the same row, since there may be some time jitter
-        // for multiple recorded data points that were updated at the same time, but network latencies
-        // or CPU usage caused the timestamps to be slightly different
-        for (; j < data.size() && data.get(j).getTimestamp() <= point.getTimestamp() + TIME_WINDOW; j++) {
-          TimestampedData d = data.get(j);
-          if (!(skipMetadata && DataSourceUtils.isMetadata(d.getSourceId()))) {
-            row.add(d);
-          }
-        }
-        Object[] rowToSave = new Object[headerSize];
-        row.forEach(d -> rowToSave[header.indexOf(d.getSourceId())] = d.getData());
-        rowToSave[0] = point.getTimestamp();
-        printer.printRecord(rowToSave);
-        i = j;
+      for (Object[] row : rows) {
+        csvPrinter.printRecord(row);
       }
+
+      return writer.toString();
+    } catch (IOException e) {
+      throw new IllegalStateException("Could not convert recording to CSV", e);
     }
+  }
+
+  private Object[] toRow(List<String> header, int headerSize, Map.Entry<Long, List<RecordingEntry>> entry) {
+    final var entries = entry.getValue();
+    if (entries.isEmpty()) {
+      return null;
+    }
+    Object[] row = new Object[headerSize];
+    row[0] = entry.getKey();
+
+    int dataStart = 0;
+
+    if (entries.get(0) instanceof Marker) {
+      dataStart = 1;
+
+      var marker = (Marker) entries.get(0);
+      row[1] = marker.getName();
+      row[2] = marker.getDescription();
+      row[3] = marker.getImportance();
+    }
+
+    for (int i = dataStart; i < entries.size(); i++) {
+      if (!(entries.get(i) instanceof TimestampedData)) {
+        // Invariant was violated, log it and discard this row
+        log.warning(
+            String.format(
+                invariantViolatedMessageFormat,
+                entries.get(i),
+                i,
+                entries.size() - 1,
+                entry.getKey()
+            )
+        );
+        return null;
+      }
+      var point = (TimestampedData) entries.get(i);
+      row[header.indexOf(point.getSourceId())] = point.getData();
+    }
+
+    return row;
   }
 
   private List<String> makeHeader(Recording recording, ConversionSettings settings) {
@@ -90,7 +143,7 @@ public final class CsvConverter implements Converter {
       header.removeIf(DataSourceUtils::isMetadata);
     }
     header.sort(AlphanumComparator.INSTANCE);
-    header.add(0, "Timestamp");
+    header.addAll(0, List.of("Timestamp", "Event", "Event Description", "Event Severity"));
     return header;
   }
 
