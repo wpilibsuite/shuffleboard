d7d5889d96308ecaa02befa9763370170bcaa46b
Kevin Clark <kevin.clark@gmail.com>
2020-03-02 17:01:21 -0800

    Allow NumberSliderWidget to display it's value
    Configurable via edit, default on

--- plugins/base/src/main/java/edu/wpi/first/shuffleboard/plugin/base/widget/NumberSliderWidget.java
+++ plugins/base/src/main/java/edu/wpi/first/shuffleboard/plugin/base/widget/NumberSliderWidget.java
@@ -10,8 +10,13 @@ import com.google.common.collect.ImmutableList;
 
 import java.util.List;
 
+import org.fxmisc.easybind.EasyBind;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Slider;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 
 @Description(
@@ -24,6 +29,10 @@ public class NumberSliderWidget extends SimpleAnnotatedWidget<Number> {
   private Pane root;
   @FXML
   private Slider slider;
+  @FXML
+  private Label text;
+
+  private final BooleanProperty showText = new SimpleBooleanProperty(this, "showText", true);
 
   @FXML
   private void initialize() {
@@ -33,6 +42,7 @@ public class NumberSliderWidget extends SimpleAnnotatedWidget<Number> {
               .subtract(slider.minProperty())
               .divide(4));
     slider.valueProperty().bindBidirectional(dataProperty());
+    text.textProperty().bind(EasyBind.map(dataOrDefault, n -> String.format("%.2f", n.doubleValue())));
   }
 
   @Override
@@ -47,7 +57,22 @@ public class NumberSliderWidget extends SimpleAnnotatedWidget<Number> {
             Setting.of("Min", slider.minProperty(), Double.class),
             Setting.of("Max", slider.maxProperty(), Double.class),
             Setting.of("Block increment", slider.blockIncrementProperty(), Double.class)
+        ),
+        Group.of("Visuals",
+            Setting.of("Display value", showText, Boolean.class)
         )
     );
+  }
+
+  public boolean isShowText() {
+    return showText.get();
+  }
+
+  public BooleanProperty showTextProperty() {
+    return showText;
+  }
+
+  public void setShowText(boolean showText) {
+    this.showText.set(showText);
   }
 }
