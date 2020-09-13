61c7cb27b790f6c52d37549ce2d65fc98fce0f81
carbotaniuman <41451839+carbotaniuman@users.noreply.github.com>
2020-09-13 15:14:54 -0500

    Use DecimalFormat to format numbers

--- api/src/main/java/edu/wpi/first/shuffleboard/api/components/NumberField.java
+++ api/src/main/java/edu/wpi/first/shuffleboard/api/components/NumberField.java
@@ -1,6 +1,9 @@
 package edu.wpi.first.shuffleboard.api.components;
 
 import java.util.regex.Pattern;
+import java.util.Locale;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 
 /**
  * A type of text field that only accepts valid floating-point decimal numbers.
@@ -9,6 +12,8 @@ public class NumberField extends AbstractNumberField<Double> {
 
   private static final Pattern startOfFloatingPointNumber = Pattern.compile("^[-+]?\\d*\\.?\\d*$");
   private static final Pattern completeFloatingPointNumber = Pattern.compile("^[-+]?\\d*\\.?\\d+$");
+  private static final DecimalFormat textFromNumberFormat =
+          new DecimalFormat("0", DecimalFormatSymbols.getInstance(Locale.ENGLISH));
 
   /**
    * Creates a new number field with no value.
@@ -33,7 +38,7 @@ public class NumberField extends AbstractNumberField<Double> {
 
   @Override
   protected String getTextFromNumber(Double num) {
-    return String.format("%f", num);
+    return textFromNumberFormat.format(num);
   }
 
   @Override
@@ -45,5 +50,4 @@ public class NumberField extends AbstractNumberField<Double> {
   protected boolean isStartOfNumber(String text) {
     return startOfFloatingPointNumber.matcher(text).matches();
   }
-
 }
